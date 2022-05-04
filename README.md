4f1cca198cf457443927e77a78e096ca4e6e0d39
gabrieltorres519 <gabrieltorres9909@gmail.com>
2022-05-04 12:25:55 -0500

    Agregado el código para crear una card en trello y documentado el proceso en el código y readme

---
+++ README.md
@@ -0,0 +1,16 @@
+Documentación de la API Trello:
+
+[API Trello](https://developer.atlassian.com/cloud/trello/rest/api-group-boards/)
+
+Documentación del cliente de Trello utilizado.
+
+[Trello client](https://github.com/norberteder/trello)
+
+
+Obtención del id de una lista de un board específico creado:
+
+![Imgur](https://i.imgur.com/WunCL7G.png)
+
+Creación de la card en la lista seleccionada con el id obtenido.
+
+![Imgur](https://i.imgur.com/uLTtWkY.png)
--- app.js
+++ app.js
@@ -6,13 +6,20 @@ if(!process.env.TOKEN && !process.env.KEY){
 }
 
 
-let Trello = require("trello");
+let Trello = require("trello"); //Cliente de trello
 let trello = new Trello(process.env.KEY, process.env.TOKEN); // Objeto de trello para autenticarnos
 
 let cardTitle = `Card Nueva desde código ${new Date()}` // Definimos texto con fecha para crear una card nueva
 
-trello.addCard(cardTitle, "LaunchX Card Creada por Gabriel Torres desde código", "6272b12a422ab2203fbbd174", // Método del objeto de Trello para permitirnos crear una card
-	function (error, trelloCard) { // Ojo el código mandado por paŕametros es el id del board que cree desde Postman, allí recuperamos el id
+trello.addCard(cardTitle, "LaunchX Card Creada por Gabriel Torres desde código", "6272b12a422ab2203fbbd175", // Método del objeto de Trello para permitirnos crear una card
+	function (error, trelloCard) { // Ojo el código mandado por paŕametros es el id de la lista "lista de tareas", en el board BoardToAddCardFromCode creada desde Postman
+        // para obtener el id de la lista se necesitó:
+        //1. Crear el board nuevo desde postman con una HTTP request
+        //2. Fijarse en el objeto que responde trello y encontrar el id del Board creado
+        //3. Usar el id del Board para obtener los ids de las listas en el board con otra HTTP request
+        //4. Reuperar el id de la lista en donde queremos crear la card 
+        //5. Pegar el id en la HTTP request aquí en código
+        // Para saber cómo realizar estas http requests en trello se vio la documentación de trello rest
 		if (error) {
 			console.log('Could not add card:', error);
 		}
